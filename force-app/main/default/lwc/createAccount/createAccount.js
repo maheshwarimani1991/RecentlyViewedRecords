9171a1126ca75a2e2db84d3d245597666b95bb2d
Himanshu <maheshwarimani1991@gmail.com>
2021-08-06 12:26:34 +0530

    Updated

--- force-app/main/default/lwc/createAccount/createAccount.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class CreateAccount extends LightningElement {
-    @api recordId;
-    @api objectApiName;
-    fields = ['Name','Type','Website'];
-}
--- force-app/main/default/lwc/createRecord/createRecord.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class CreateRecord extends LightningElement {
-    @api recordId;
-    @api objectApiName;
-    fields = ['AccountId', 'Name', 'Title', 'Phone', 'Email'];
-}
